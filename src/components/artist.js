93ff07ec018c350008a07ab59e643880e7a71bd2
2Kier16 <99277532+2Kier16@users.noreply.github.com>
2022-06-06 16:44:21 -0400

    second update

--- src/components/artist.js
+++ src/components/artist.js
@@ -4,7 +4,9 @@ export default function Artist() {
   return (
     <div className="fav-artist">
       <div className="artist-list">
-        <div className="list-item"></div>
+        <div className="list-item">
+          <h1> place name and pick1</h1>
+        </div>
       </div>
     </div>
   );
--- src/components/songs.js
+++ src/components/songs.js
@@ -4,7 +4,9 @@ export default function Songs() {
   return (
     <div className="fav-songs">
       <div className="songs-list">
-        <div className="list-item"></div>
+        <div className="list-item">
+          <h1>song and album</h1>
+        </div>
       </div>
     </div>
   );
