4619e56b81dd119b8758072d529e1dc487e94b18
2Kier16 <99277532+2Kier16@users.noreply.github.com>
2022-06-06 22:45:33 -0400

    update styles and added images

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "../style/main.scss";
-
+import notes from "../../static/assets/images/weekly goal images/music.jpg";
 // create a componet for your second page(Songs&Albums/artist)
 // make a route for that Component
 // add a navbar for both your app and the component
@@ -11,6 +11,7 @@ export default class App extends Component {
       <div className="app body">
         <div className="text-content">
           <h1>Kier's List of Fav Artist & Songs</h1>
+          <img src={notes} />
         </div>
       </div>
     );
